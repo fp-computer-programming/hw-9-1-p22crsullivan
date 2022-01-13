04bdf17adda423847f2f2fddf800271f182b5786
p22crsullivan <p22crsullivan@fairfieldprep.org>
2022-01-13 10:05:35 -0500

    Submit HW 9-1-3

--- hw9-1-3.py
+++ hw9-1-3.py
@@ -1,10 +1,14 @@
 # Author: CRS 01/12/22
 def find_thing(lst_or_string, to_be_found):
-    for to_be_found, to_be_found in enumerate(lst_or_string):
-        if to_be_found in lst_or_string:
-            print(lst_or_string)
-        elif to_be_found not in lst_or_string:
-            print(-1)
-
+    for i, v in enumerate(lst_or_string):
+        try:
+            if to_be_found in v:
+                print(i)
+                break
+            elif to_be_found not in lst_or_string:
+                print(-1)
+                break
+        except:
+            print("Invalid Input")
 
 find_thing("apple", "p")
